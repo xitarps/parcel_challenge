38a53cec735169e66b69259321473cfe90d48a29
xita Rps <xitarps@gmail.com>
2020-10-26 02:21:49 -0300

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-FactoryBot.create(:admin, email:'admin@test.com', password: 'admin123')
+Admin.create(email:'admin@test.com', password: 'admin123')
