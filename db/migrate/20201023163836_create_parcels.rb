521d5be1a02d8ae2aae0461ef464b91801e9663d
xita Rps <xitarps@gmail.com>
2020-10-26 02:08:46 -0300

    Heroku fix scale and capitalize

--- db/migrate/20201023163836_create_parcels.rb
+++ db/migrate/20201023163836_create_parcels.rb
@@ -2,7 +2,7 @@ class CreateParcels < ActiveRecord::Migration[6.0]
   def change
     create_table :parcels do |t|
       t.integer :position, null: false
-      t.decimal :value, precision: 10, scale: 12, null: false
+      t.decimal :value, precision: 10, scale: 10, null: false
       t.date :expiring_date, null: false
       t.boolean :paid, null: false, default: false
       t.references :credit, null: false, foreign_key: true
