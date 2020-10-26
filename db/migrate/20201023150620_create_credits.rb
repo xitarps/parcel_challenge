97b2c7924ca8ce25efe3db5d5b6e7855e956cd83
xita Rps <xitarps@gmail.com>
2020-10-26 03:16:44 -0300

    Fix placehoders

--- db/migrate/20201023150620_create_credits.rb
+++ db/migrate/20201023150620_create_credits.rb
@@ -1,11 +1,11 @@
 class CreateCredits < ActiveRecord::Migration[6.0]
   def change
     create_table :credits do |t|
-      t.decimal :parcel, precision: 10, scale: 10, default: '0.0', null: false
-      t.decimal :tax, precision: 10, scale: 10, null: false
+      t.decimal :parcel, precision: 10, scale: 12, default: '0.0', null: false
+      t.decimal :tax, precision: 10, scale: 12, null: false
       t.integer :periods, null: false
       t.boolean :already_accepted, null: false, default: false
-      t.decimal :loan, precision: 10, scale: 10, null: false
+      t.decimal :loan, precision: 10, scale: 12, null: false
       t.references :requester, null: false, foreign_key: true
 
       t.timestamps
